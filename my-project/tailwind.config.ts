4df3616968c2f0e30f3ebca296167375406989c9
VishnuWD <vishnuyadavhome@gmail.com>
2023-09-17 05:08:32 +0530

    fifth commit changed tailwind config

--- my-project/tailwind.config.ts
+++ my-project/tailwind.config.ts
@@ -1,6 +1,6 @@
+import type { Config } from 'tailwindcss'
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -17,4 +17,4 @@ module.exports = {
   },
   plugins: [],
 }
-
+export default config
