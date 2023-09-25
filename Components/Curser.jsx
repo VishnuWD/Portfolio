4af571fb94f4647333481e0e58b4510e83996e06
VishnuWD <vishnuyadavhome@gmail.com>
2023-09-25 23:36:51 +0530

    update some miner changes of curser of commit 2

--- Components/Curser.jsx
+++ Components/Curser.jsx
@@ -1,22 +1,18 @@
 import React, { useEffect } from "react";
 
-const Curser = () => {
+const Cursor = () => {
   useEffect(() => {
     const handleMouseMove = (event) => {
       const first = document.getElementById("first");
       const second = document.getElementById("second");
 
- 
       if (first && second) {
-        first.style.left = event.x  + "px";
-        first.style.top = event.y  + "px";
+        first.style.left = event.x + "px";
+        first.style.top = event.y + "px";
 
-      
-        first.style.left = event.x - 5 + "px";
-        first.style.top = event.y - 5 + "px";
         second.style.left = event.x - 100 + "px";
         second.style.top = event.y - 100 + "px";
-      
+      }
     };
 
     const loopFunction = () => {
@@ -24,29 +20,23 @@ const Curser = () => {
       const topRight = Math.floor(Math.random() * 40 + 20) + "vmin";
       const bottomRight = Math.floor(Math.random() * 40 + 20) + "vmin";
       const bottomLeft = Math.floor(Math.random() * 40 + 20) + "vmin";
-      //   const height = Math.floor(Math.random() * 100 + 100) + "px";
-      //   const width = Math.floor(Math.random() * 100 + 100) + "px";
 
-      // Set the borderRadius property with the correct values
       second.style.borderRadius = `${topLeft} ${topRight} ${bottomRight} ${bottomLeft}/ ${topLeft} ${topRight} ${bottomRight} ${bottomLeft}`;
       second.style.height = "30vmin";
       second.style.width = "30vmin";
       second.style.transition = "all 0.5s linear";
-        // second.style.transition = "width 1s ease-in-out";
-
-      //   second.style.transition = "height 1s ease-in-out";
     };
 
     const intervalId = setInterval(loopFunction, 5000);
 
-    // Attach the event listener when the component mounts
     document.addEventListener("mousemove", handleMouseMove);
 
-    // Remove the event listener when the component unmounts
     return () => {
+      clearInterval(intervalId); 
       document.removeEventListener("mousemove", handleMouseMove);
     };
-}}, []);
+  }, []);
+
   return (
     <div id="total">
       <div id="first"></div>
@@ -55,4 +45,5 @@ const Curser = () => {
   );
 };
 
-export default Curser;
+export default Cursor;
+
